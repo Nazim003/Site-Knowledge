b725a2d3c78d4dd63ac10cd2c185d255f98d1130
Nazim003 <87828106+Nazim003@users.noreply.github.com>
2021-07-23 10:18:10 +0600

    Update test.py

--- test.py
+++ test.py
@@ -13,9 +13,6 @@ url =raw_input("Enter your full website link with https : ")
 url.rstrip ( )
 header = urllib2.urlopen (url) .info( )
 print(str(header))
-except:
-  print(error + "No Internet connection")
-  exit
- break
-elif (num == "5"):
-  exit(5)
+num = input()
+if num == "0":
+  exit()
