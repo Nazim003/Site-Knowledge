37ca78670681166c09b7180ccf9f30be194fc773
Nazim003 <87828106+Nazim003@users.noreply.github.com>
2021-07-23 09:08:37 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+print("\033[90m")
+print("Hello")
+print("\033[89m")
+import os
+import urllib2
+import sys
+print("\033[92m")
+os.system("figlet site Knowledge ")
+print("\033[94m")
+print("Coded by: MD.NAZIM")
+print("From Bangladesh")
+print("\033[96m")
+print("Tools name is 'Site Knowledge'")
+print("\033[98m")
+url =raw_input("Enter your full website link with https : ")
+url.rstrip ( )
+header = urllib2.urlopen (url) .info( )
+print(str(header))
+
+
+
